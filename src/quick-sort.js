c96b8729ed5950e7861a4ec2af89f3177bb8425e
Sreenivas Parimi <Sreenivas_Parimi@epam.com>
2017-07-12 15:08:22 +0530

    added updated quick-sort

--- src/quick-sort.js
+++ src/quick-sort.js
@@ -1,6 +1,26 @@
 'use strict';
 
 function quickSort(input) {
-
+    if(input == null){
+        throw "err";
+    }
+    
+    if(!(input  instanceof Array)){
+        throw "Invalid Argument. Accpets only arrays";
+    }
+    
+    if(input.length <= 1){
+        return input;
+    }
+    
+    var pivot = input[0];
+    var left =[];
+    var right = [];
+    
+    for(var i = 1; i< input.length; i++){
+        input[i] < pivot ? left.push(input[i]) : right.push(input[i]);        
+    }
+    
+    return quickSort(left).concat(pivot, quickSort(right));
 };
 module.exports = quickSort;
